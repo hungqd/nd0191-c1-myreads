799ca5bcbf295a9f1e45097a974b30ddcad1e7bd
Đặng Quang Hưng <hungdq58@gmail.com>
2024-06-25 10:48:04 +0700

    Books search

--- starter/src/Book.jsx
+++ starter/src/Book.jsx
@@ -1,12 +1,15 @@
 import { update } from "./BooksAPI";
 import { shelf } from "./Constants";
 
-export default function Book({ book }) {
+export default function Book({ book, onUpdated }) {
 
     function onChangeShelf(event) {
         const shelf = event.target.value;
         update(book, shelf).then((res) => {
             console.log("Book updated. " + JSON.stringify(res));
+            if (onUpdated) {
+                onUpdated(shelf);
+            }
         });
     }
 
@@ -18,12 +21,12 @@ export default function Book({ book }) {
                     style={{
                         width: 128,
                         height: 193,
-                        backgroundImage: `url("${book.imageLinks.thumbnail}")`,
+                        backgroundImage: `url("${book.imageLinks ? book.imageLinks.thumbnail : null}")`,
                     }}
                 ></div>
                 <div className="book-shelf-changer">
-                    <select value={book.shelf} onChange={onChangeShelf}>
-                        <option value="none" disabled>
+                    <select value={book.shelf ? book.shelf : 'none'} onChange={onChangeShelf}>
+                        <option disabled>
                             Move to...
                         </option>
                         {Object.entries(shelf).map(([key, value]) => {
@@ -40,7 +43,7 @@ export default function Book({ book }) {
                 </div>
             </div>
             <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.authors.join("; ")}</div>
+            <div className="book-authors">{book.authors ? book.authors.join("; ") : null}</div>
         </div>
     );
 }
--- starter/src/BookSearch.jsx
+++ starter/src/BookSearch.jsx
@@ -1,4 +1,29 @@
+import { useState } from "react";
+import { search } from "./BooksAPI";
+import Book from "./Book";
+
+
 export default function BookSearch({ onClose }) {
+
+    const [books, setBooks] = useState([]);
+
+    function onSearch(event) {
+        const query = event.target.value.trim();
+        if (query.length > 0) {
+            search(query, 10).then((books) => {
+                setBooks(books);
+            });
+        } else {
+            setBooks([]);
+        }
+    }
+
+    function onBookUpdated(book, shelf) {
+        setBooks(books.map((b) => {
+            return b.id === book.id ? Object.assign({}, b, { shelf }) : b;
+        }));
+    }
+
     return (
         <div className="search-books">
             <div className="search-books-bar">
@@ -13,11 +38,23 @@ export default function BookSearch({ onClose }) {
                     <input
                         type="text"
                         placeholder="Search by title, author, or ISBN"
+                        onChange={onSearch}
                     />
                 </div>
             </div>
             <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                    {books.map((book) => {
+                        return (
+                            <li key={book.id}>
+                                <Book
+                                    book={book}
+                                    onUpdated={(shelf) => onBookUpdated(book, shelf)}
+                                />
+                            </li>
+                        );
+                    })}
+                </ol>
             </div>
         </div>
     );
